7757b77699bee886a2cd5e949c4f94a5f9e4f9ad
SameerJS6 <singh6.sameer@gmail.com>
2025-02-02 20:59:32 +0530

    add: Unit Tests Project

---
+++ Community.PowerToys.Run.Plugin.SVGL.UnitTests/MainTests.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Community.PowerToys.Run.Plugin.SVGL.UnitTests {
+    public class MainTests { }
+}
