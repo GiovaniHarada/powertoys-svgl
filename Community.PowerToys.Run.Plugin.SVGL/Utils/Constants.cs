724369ca33acb3093b5544dd8f3a8ee176929164
SameerJS6 <singh6.sameer@gmail.com>
2025-02-09 02:03:32 +0530

    add: context menu's Title, and baseURL's in constants

---
+++ Community.PowerToys.Run.Plugin.SVGL/Utils/Constants.cs
@@ -0,0 +1,28 @@
+﻿namespace Community.PowerToys.Run.Plugin.SVGL;
+
+
+public static class Constants
+{
+
+    public const string RequestLogo = "Request Logo";
+    public const string RequestLogoURL = "https://github.com/pheralb/svgl/issues/new?assignees=&labels=request&projects=&template=request-svg.yml&title=%5B%F0%9F%94%94+Request+SVG%5D%3A";
+
+    public const string SubmitLogo = "Submit Logo";
+    public const string SubmitLogoURL = "https://github.com/pheralb/svgl#-getting-started";
+
+    public const string APIBaseURL = "https://api.svgl.app";
+    public const string SVGLBaseURL = "https://svgl.app/library/";
+
+    public const string OpenInBrowserMessage = "Open in Browser (Enter)";
+
+    public const string CopySVGLogoMessage = "Copy Logo SVG (Enter)";
+    public const string CopySVGWordmarkMessage = "Copy Wordmark SVG (Ctrl + Enter)";
+
+    public const string CopyLightThemeSVGLogoMessage = "Copy Light Theme Logo (Enter)";
+    public const string CopyDarkThemeSVGLogoMessage = "Copy Dark Theme Logo (Ctrl + Enter)";
+
+    public const string CopyLightThemeSVGWordmarMessage = "Copy Light Theme Wordmark (Shift + Enter)";
+    public const string CopyDarkThemeSVGWordmarMessage = "Copy Dark Theme Wordmark (Ctrl + Shift + Enter)";
+
+
+}
