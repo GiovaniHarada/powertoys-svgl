6393a647ba2e0b97d002b675029255a1d588f6aa
SameerJS6 <singh6.sameer@gmail.com>
2025-02-09 02:04:04 +0530

    add: `CopySVGContent` Context Action Utility

---
+++ Community.PowerToys.Run.Plugin.SVGL/Utils/utils.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace Community.PowerToys.Run.Plugin.SVGL;
+
+public class Utils
+{
+    private static readonly MyApiClients _apiClient = new MyApiClients();
+
+    public static bool CopyToClipboard(string value)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            Clipboard.SetText(value);
+            return true;
+        }
+        return false;
+    }
+
+    public static bool CopySVGContent(string svg)
+    {
+        //var apiClient = new MyApiClients();
+        var content = Task.Run(async () => await _apiClient.GetSVGContent(svg)).Result;
+        CopyToClipboard(content);
+        return true;
+    }
+
+}
