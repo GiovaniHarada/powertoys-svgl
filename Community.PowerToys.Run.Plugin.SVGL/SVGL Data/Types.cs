56fa9c9f393682a1a38b7137d875b2f2ed2e57e6
SameerJS6 <singh6.sameer@gmail.com>
2025-02-09 01:01:40 +0530

    Reformatted namespace and class definitions for clarity. Improved indentation and spacing in multiple classes.

--- Community.PowerToys.Run.Plugin.SVGL/SVGL Data/Types.cs
+++ Community.PowerToys.Run.Plugin.SVGL/SVGL Data/Types.cs
@@ -1,96 +1,95 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace Community.PowerToys.Run.Plugin.SVGL
+namespace Community.PowerToys.Run.Plugin.SVGL;
+
+public class SVGL
 {
-    public class SVGL
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("category")]
+    [JsonConverter(typeof(CategoryBaseConverter))]
+    public CategoryBase Category { get; set; }
+
+    [JsonPropertyName("route")]
+    [JsonConverter(typeof(ThemeBaseConverter))]
+    public ThemeBase Route { get; set; }
+
+    [JsonPropertyName("wordmark")]
+    [JsonConverter(typeof(ThemeBaseConverter))]
+    public ThemeBase Wordmark { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+}
+
+public abstract class CategoryBase
+{ }
+
+public class CategoryString : CategoryBase
+{
+    public string Category { get; set; }
+
+    public CategoryString(string category)
     {
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
-
-        [JsonPropertyName("title")]
-        public string Title { get; set; }
-
-        [JsonPropertyName("category")]
-        [JsonConverter(typeof(CategoryBaseConverter))]
-        public CategoryBase Category { get; set; }
-
-        [JsonPropertyName("route")]
-        [JsonConverter(typeof(ThemeBaseConverter))]
-        public ThemeBase Route { get; set; }
-
-        [JsonPropertyName("wordmark")]
-        [JsonConverter(typeof(ThemeBaseConverter))]
-        public ThemeBase Wordmark { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
+        Category = category;
     }
 
-    public abstract class CategoryBase
-    { }
+    public override string ToString() => Category;
+}
 
-    public class CategoryString : CategoryBase
+public class CategoryArray : CategoryBase
+{
+    public List<string> Categories { get; set; }
+
+    public CategoryArray(List<string> categories)
     {
-        public string Category { get; set; }
-
-        public CategoryString(string category)
-        {
-            Category = category;
-        }
-
-        public override string ToString() => Category;
+        Categories = categories;
     }
 
-    public class CategoryArray : CategoryBase
+    public override string ToString() => string.Join(", ", Categories);
+}
+
+public class ThemeBase { }
+
+public class ThemeString : ThemeBase
+{
+    public string Route { get; set; }
+
+    public ThemeString(string route)
     {
-        public List<string> Categories { get; set; }
-
-        public CategoryArray(List<string> categories)
-        {
-            Categories = categories;
-        }
-
-        public override string ToString() => string.Join(", ", Categories);
+        Route = route;
     }
 
-    public class ThemeBase { }
+    public override string ToString() => Route;
+}
 
-    public class ThemeString : ThemeBase
+public class ThemeObject : ThemeBase
+{
+    public SVGThemes Route { get; set; }
+
+    public ThemeObject(SVGThemes route)
     {
-        public string Route { get; set; }
-
-        public ThemeString(string route)
-        {
-            Route = route;
-        }
-
-        public override string ToString() => Route;
+        Route = route;
     }
 
-    public class ThemeObject : ThemeBase
-    {
-        public SVGThemes Route { get; set; }
+    public override string ToString() => $"Light: {Route.Light}, Dark: {Route.Dark}";
+}
 
-        public ThemeObject(SVGThemes route)
-        {
-            Route = route;
-        }
+public class SVGThemes
+{
+    [JsonPropertyName("light")]
+    public string Light { get; set; }
+    [JsonPropertyName("dark")]
+    public string Dark { get; set; }
+}
 
-        public override string ToString() => $"Light: {Route.Light}, Dark: {Route.Dark}";
-    }
-
-    public class SVGThemes
-    {
-        [JsonPropertyName("light")]
-        public string Light { get; set; }
-        [JsonPropertyName("dark")]
-        public string Dark { get; set; }
-    }
-
-    public class INavigateToBrowserData
-    {
-        public string Identifier { get; set; }
-        public string Search { get; set; }
-    }
+public class INavigateToBrowserData
+{
+    public string Identifier { get; set; }
+    public string Search { get; set; }
 }
