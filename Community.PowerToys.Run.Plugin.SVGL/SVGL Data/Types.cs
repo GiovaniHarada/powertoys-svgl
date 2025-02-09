64da88f28a89ea5b1dc0b8f8bb596871956cdf90
SameerJS6 <singh6.sameer@gmail.com>
2025-02-10 01:12:13 +0530

    add: `GetContextMenuResult` Types

--- Community.PowerToys.Run.Plugin.SVGL/SVGL Data/Types.cs
+++ Community.PowerToys.Run.Plugin.SVGL/SVGL Data/Types.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using System.Windows.Input;
+using Wox.Plugin;
 
 namespace Community.PowerToys.Run.Plugin.SVGL;
 
@@ -92,4 +95,14 @@ public class INavigateToBrowserData
 {
     public string Identifier { get; set; }
     public string Search { get; set; }
+}
+
+public class IGetContextMenuResult
+{
+    public string Title { get; set; }
+    public string Glyph { get; set; }
+    public Key AcceleratorKey { get; set; }
+    public ModifierKeys AcceleratorModifiers { get; set; }
+    public string CopyContent { get; set; }
+    public Func<ActionContext, bool> CustomAction { get; set; }
 }
