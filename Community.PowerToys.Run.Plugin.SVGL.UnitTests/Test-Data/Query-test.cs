e3de0504f240b8439c8929aad757426db0f99997
SameerJS6 <singh6.sameer@gmail.com>
2025-02-19 02:35:53 +0530

    add: Context Menu Item Data

--- Community.PowerToys.Run.Plugin.SVGL.UnitTests/Test-Data/Query-test.cs
+++ Community.PowerToys.Run.Plugin.SVGL.UnitTests/Test-Data/Query-test.cs
@@ -1,140 +1,152 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Input;
 using Wox.Plugin;
 
 
 namespace Community.PowerToys.Run.Plugin.SVGL.UnitTests;
+
 public static class QueryTestData
 {
     public static List<Result> GoogleData()
     {
-        List<Result> results = new List<Result>{new Result
+        var results = new List<Result>
         {
-            Title = "Google",
-            SubTitle = TestContants.expectedCategoryString("Google").ToString(),
-            IcoPath = "Images\\svgl.dark.png",
-            Score = 100,
-            ContextData = new SVGL
+            new()
             {
-                Id = 1,
                 Title = "Google",
-                Category = TestContants.expectedCategoryString("Google"),
-                Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
-                Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
-                Url = "https://google.com"
+                SubTitle = TestContants.expectedCategoryString("Google").ToString(),
+                IcoPath = "Images\\svgl.dark.png",
+                Score = 100,
+                ContextData = new Svgl
+                {
+                    Id = 1,
+                    Title = "Google",
+                    Category = TestContants.expectedCategoryString("Google"),
+                    Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
+                    Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
+                    Url = "https://google.com"
+                },
+                Action = _ => true
             },
-            Action = _ => true
-        },
-        new Result{
-            Title = "Google Drive",
-            SubTitle = TestContants.expectedCategoryString("Google").ToString(),
-            IcoPath = "Images\\svgl.dark.png",
-            Score = 99,
-            ContextData = new SVGL
+            new()
             {
-                Id = 1,
-                Title = "Google",
-                Category = TestContants.expectedCategoryString("Google"),
-                Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
-                Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
-                Url = "https://google.com"
+                Title = "Google Drive",
+                SubTitle = TestContants.expectedCategoryString("Google").ToString(),
+                IcoPath = "Images\\svgl.dark.png",
+                Score = 99,
+                ContextData = new Svgl
+                {
+                    Id = 1,
+                    Title = "Google",
+                    Category = TestContants.expectedCategoryString("Google"),
+                    Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
+                    Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
+                    Url = "https://google.com"
+                },
+                Action = _ => true
             },
-            Action = _ => true
-        },
-        new Result{
-            Title = "Google Idx",
-            SubTitle = $"Software, {TestContants.expectedCategoryString("Google").ToString()}",
-            IcoPath = "Images\\svgl.dark.png",
-            Score = 98,
-            ContextData = new SVGL
+            new()
             {
-                Id = 1,
-                Title = "Google",
-                Category = TestContants.expectedCategoryString("Google"),
-                Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
-                Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
-                Url = "https://google.com"
+                Title = "Google Idx",
+                SubTitle = $"Software, {TestContants.expectedCategoryString("Google").ToString()}",
+                IcoPath = "Images\\svgl.dark.png",
+                Score = 98,
+                ContextData = new Svgl
+                {
+                    Id = 1,
+                    Title = "Google",
+                    Category = TestContants.expectedCategoryString("Google"),
+                    Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
+                    Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
+                    Url = "https://google.com"
+                },
+                Action = _ => true
             },
-            Action = _ => true
-        },
-        new Result{
-            Title = "Google PaLM",
-            SubTitle = $"AI, {TestContants.expectedCategoryString("Google").ToString()}",
-            IcoPath = "Images\\svgl.dark.png",
-            Score = 97,
-            ContextData = new SVGL
+            new()
             {
-                Id = 1,
-                Title = "Google",
-                Category = TestContants.expectedCategoryString("Google"),
-                Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
-                Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
-                Url = "https://google.com"
+                Title = "Google PaLM",
+                SubTitle = $"AI, {TestContants.expectedCategoryString("Google").ToString()}",
+                IcoPath = "Images\\svgl.dark.png",
+                Score = 97,
+                ContextData = new Svgl
+                {
+                    Id = 1,
+                    Title = "Google",
+                    Category = TestContants.expectedCategoryString("Google"),
+                    Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
+                    Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
+                    Url = "https://google.com"
+                },
+                Action = _ => true
             },
-            Action = _ => true
-        },
-        new Result{
-            Title = "Google Colaboratory",
-            SubTitle = $"{TestContants.expectedCategoryString("Google").ToString()}, Software",
-            IcoPath = "Images\\svgl.dark.png",
-            Score = 96,
-            ContextData = new SVGL
+            new()
             {
-                Id = 1,
-                Title = "Google",
-                Category = TestContants.expectedCategoryString("Google"),
-                Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
-                Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
-                Url = "https://google.com"
-            },
-            Action = _ => true
-        }};
+                Title = "Google Colaboratory",
+                SubTitle = $"{TestContants.expectedCategoryString("Google").ToString()}, Software",
+                IcoPath = "Images\\svgl.dark.png",
+                Score = 96,
+                ContextData = new Svgl
+                {
+                    Id = 1,
+                    Title = "Google",
+                    Category = TestContants.expectedCategoryString("Google"),
+                    Route = TestContants.expectedRouteString("https://svgl.app/library/google.svg"),
+                    Wordmark = TestContants.expectedWordmarkString("https://svgl.app/library/google-wordmark.svg"),
+                    Url = "https://google.com"
+                },
+                Action = _ => true
+            }
+        };
 
         return results;
     }
 
     public static List<Result> FlareData()
     {
-        List<Result> results = new List<Result>{new Result
+        var results = new List<Result>
         {
-            Title = "Cloudflare Workers",
-            SubTitle = TestContants.expectedCategoryString("Software").ToString(),
-            IcoPath = "Images\\svgl.dark.png",
-            Score = 100,
-            ContextData = new SVGL
+            new()
             {
-                Id = 179,
                 Title = "Cloudflare Workers",
-                Category = TestContants.expectedCategoryString("Software"),
-                Route = TestContants.expectedRouteString("https://svgl.app/library/cloudflare-workers.svg"),
-                Url = "https://workers.cloudflare.com/"
+                SubTitle = TestContants.expectedCategoryString("Software").ToString(),
+                IcoPath = "Images\\svgl.dark.png",
+                Score = 100,
+                ContextData = new Svgl
+                {
+                    Id = 179,
+                    Title = "Cloudflare Workers",
+                    Category = TestContants.expectedCategoryString("Software"),
+                    Route = TestContants.expectedRouteString("https://svgl.app/library/cloudflare-workers.svg"),
+                    Url = "https://workers.cloudflare.com/"
+                },
+                Action = _ => true
             },
-            Action = _ => true
-        },
-        new Result{
-            Title = "Cloudflare",
-            SubTitle = TestContants.expectedCategoryString("Software").ToString(),
-            IcoPath = "Images\\svgl.dark.png",
-            Score = 99,
-            ContextData = new SVGL
+            new()
             {
-                Id = 180,
                 Title = "Cloudflare",
-                Category = TestContants.expectedCategoryString("Software"),
-                Route = TestContants.expectedRouteString("https://svgl.app/library/cloudflare.svg"),
-                Url = "https://www.cloudflare.com/"
-            },
-            Action = _ => true
-        },
-       };
+                SubTitle = TestContants.expectedCategoryString("Software").ToString(),
+                IcoPath = "Images\\svgl.dark.png",
+                Score = 99,
+                ContextData = new Svgl
+                {
+                    Id = 180,
+                    Title = "Cloudflare",
+                    Category = TestContants.expectedCategoryString("Software"),
+                    Route = TestContants.expectedRouteString("https://svgl.app/library/cloudflare.svg"),
+                    Url = "https://www.cloudflare.com/"
+                },
+                Action = _ => true
+            }
+        };
 
         return results;
     }
 
     public static List<Result> NoResultFoundData(string query)
     {
-        List<Result> results = new List<Result>
+        var results = new List<Result>
         {
-            new Result
+            new()
             {
                 Title = TestContants.NoResultFoundMessage,
                 SubTitle = TestContants.NoResultFoundSubTitle(query),
@@ -142,14 +154,14 @@ public static class QueryTestData
                 Score = 100,
                 Action = _ => false
             },
-            new Result
+            new()
             {
                 Title = TestContants.RequestLogo,
                 SubTitle = "Request a Logo on SVGL's Repository",
                 IcoPath = "Images\\svgl.dark.png",
                 Action = _ => true
             },
-            new Result
+            new()
             {
                 Title = TestContants.SubmitLogo,
                 SubTitle = "Submit a Logo on SVGL's Repository",
@@ -160,54 +172,137 @@ public static class QueryTestData
         return results;
     }
 
-    public static List<SVGL> GetSampleSvgs()
+    public static List<Svgl> GetSampleSvgs()
     {
-        return new List<SVGL>
+        return
+        [
+            // Case 1: Simple case - ThemeString + CategoryString
+            new Svgl
+            {
+                Id = 1,
+                Title = "Google",
+                Category = new CategoryString("Technology"),
+                Route = new ThemeString("google.svg"),
+                Wordmark = new ThemeString("google-wordmark.svg"),
+                Url = "https://google.com"
+            },
+
+            // Case 2: Theme with light/dark + Array of categories
+            new Svgl
+            {
+                Id = 2,
+                Title = "Vercel",
+                Category = new CategoryArray(["Hosting", "Development"]),
+                Route = new ThemeObject(new SvgThemes
+                {
+                    Light = "vercel.svg",
+                    Dark = "vercel-dark.svg"
+                }),
+                Wordmark = new ThemeObject(new SvgThemes
+                {
+                    Light = "vercel-wordmark.svg",
+                    Dark = "vercel-wordmark-dark.svg"
+                }),
+                Url = "https://vercel.com"
+            },
+
+            // Case 3: Mixed theme types + single category
+            new Svgl
+            {
+                Id = 3,
+                Title = "GitHub",
+                Category = new CategoryString("Development"),
+                Route = new ThemeObject(new SvgThemes
+                {
+                    Light = "github.svg",
+                    Dark = "github-dark.svg"
+                }),
+                Wordmark = new ThemeString("github-wordmark.svg"),
+                Url = "https://github.com"
+            }
+        ];
+    }
+
+
+    public static Svgl GetSampleSVGContextData()
     {
-        // Case 1: Simple case - ThemeString + CategoryString
-        new SVGL
-        {
-            Id = 1,
-            Title = "Google",
-            Category = new CategoryString("Technology"),
-            Route = new ThemeString("google.svg"),
-            Wordmark = new ThemeString("google-wordmark.svg"),
-            Url = "https://google.com"
-        },
-
-        // Case 2: Theme with light/dark + Array of categories
-        new SVGL
-        {
-            Id = 2,
-            Title = "Vercel",
-            Category = new CategoryArray(["Hosting", "Development"]),
-            Route = new ThemeObject(new SVGThemes
+        return
+            new Svgl()
             {
-                Light = "vercel.svg",
-                Dark = "vercel-dark.svg"
-            }),
-            Wordmark = new ThemeObject(new SVGThemes
-            {
-                Light = "vercel-wordmark.svg",
-                Dark = "vercel-wordmark-dark.svg"
-            }),
-            Url = "https://vercel.com"
-        },
+                Id = 2,
+                Title = "Vercel",
+                Category = new CategoryArray(["Hosting", "Development"]),
+                Route = new ThemeObject(new SvgThemes
+                {
+                    Light = "vercel.svg",
+                    Dark = "vercel-dark.svg"
+                }),
+                Wordmark = new ThemeObject(new SvgThemes
+                {
+                    Light = "vercel-wordmark.svg",
+                    Dark = "vercel-wordmark-dark.svg"
+                }),
+                Url = "https://vercel.com"
+            }
+            ;
+    }
 
-        // Case 3: Mixed theme types + single category
-        new SVGL
+    public static Svgl GetDefaultSVGContextData()
+    {
+        return new Svgl
         {
             Id = 3,
             Title = "GitHub",
             Category = new CategoryString("Development"),
-            Route = new ThemeObject(new SVGThemes
-            {
-                Light = "github.svg",
-                Dark = "github-dark.svg"
-            }),
-            Wordmark = new ThemeString("github-wordmark.svg"),
+            Route = new ThemeString("github.svg"),
             Url = "https://github.com"
-        }
-    };
+        };
+    }
+
+    public static List<ContextMenuResult> GetDefaultContextMenu()
+    {
+        return
+        [
+            new ContextMenuResult
+            {
+                Title = Constants.CopySvgLogoMessage,
+                Glyph = "\xE8C8",
+                AcceleratorKey = Key.Enter
+            }
+        ];
+    }
+
+    public static List<ContextMenuResult> GetAllContextMenu()
+    {
+        return
+        [
+            new ContextMenuResult
+            {
+                Title = Constants.CopyLightThemeSvgLogoMessage,
+                Glyph = "\xE706",
+                AcceleratorKey = Key.Enter
+            },
+            new ContextMenuResult
+            {
+                Title = Constants.CopyDarkThemeSvgLogoMessage,
+                Glyph = "\xE708",
+                AcceleratorKey = Key.Enter,
+                AcceleratorModifiers = ModifierKeys.Control
+            },
+            new ContextMenuResult
+            {
+                Title = Constants.CopyLightThemeSvgWordmarkMessage,
+                Glyph = "\xE8D2",
+                AcceleratorKey = Key.Enter,
+                AcceleratorModifiers = ModifierKeys.Shift
+            },
+            new ContextMenuResult
+            {
+                Title = Constants.CopyDarkThemeSvgWordmarkMessage,
+                Glyph = "\xE8D3",
+                AcceleratorKey = Key.Enter,
+                AcceleratorModifiers = ModifierKeys.Control | ModifierKeys.Shift
+            }
+        ];
     }
 }
