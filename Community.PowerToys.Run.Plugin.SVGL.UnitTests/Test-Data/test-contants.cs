b39843cd29a0cd48c7a71415d1710271f05de2c2
SameerJS6 <singh6.sameer@gmail.com>
2025-02-20 01:11:16 +0530

    refactor: improve constant variables & using its own namespace rather than the main namespace.

--- Community.PowerToys.Run.Plugin.SVGL.UnitTests/Test-Data/test-contants.cs
+++ Community.PowerToys.Run.Plugin.SVGL.UnitTests/Test-Data/test-contants.cs
@@ -1,43 +1,43 @@
-﻿namespace Community.PowerToys.Run.Plugin.SVGL.UnitTests
+﻿namespace Community.PowerToys.Run.Plugin.SVGL.UnitTests.Test_Data;
+
+internal abstract class TestConstants
 {
-    internal class TestContants
+    public static ThemeString ExpectedRouteString(string content)
     {
-        public static ThemeString expectedRouteString(string content)
-        {
-            return new ThemeString(content);
-        }
-
-        public static ThemeString expectedWordmarkString(string content)
-        {
-            return new ThemeString(content);
-        }
-
-        public static ThemeObject expectedRouteObject = new ThemeObject(new SVGThemes
-        {
-            Dark = "https://svgl.app/library/vercel_dark.svg",
-            Light = "https://svgl.app/library/vercel.svg"
-        });
-        public static ThemeObject expectedWordmarkObject = new ThemeObject(new SVGThemes
-        {
-            Light = "https://svgl.app/library/vercel_wordmark.svg",
-            Dark = "https://svgl.app/library/vercel_wordmark_dark.svg",
-        });
-
-        public static CategoryString expectedCategoryString(string category)
-        {
-            return new CategoryString(category);
-        }
-
-        public static CategoryArray expectedCategoryArray = new CategoryArray(["Hosting", "Vercel"]);
-
-        public static string NoResultFoundMessage = "No SVG Found";
-        public static string NoResultFoundSubTitle(string query)
-        {
-            return $"Could not found {query} SVG";
-        }
-
-        public const string RequestLogo = "Request Logo";
-        public const string SubmitLogo = "Submit Logo";
-
+        return new ThemeString(content);
     }
+
+    public static ThemeString ExpectedWordmarkString(string content)
+    {
+        return new ThemeString(content);
+    }
+
+    public static ThemeObject ExpectedRouteObject = new(new SvgThemes
+    {
+        Dark = "https://svgl.app/library/vercel_dark.svg",
+        Light = "https://svgl.app/library/vercel.svg"
+    });
+
+    public static ThemeObject ExpectedWordmarkObject = new(new SvgThemes
+    {
+        Light = "https://svgl.app/library/vercel_wordmark.svg",
+        Dark = "https://svgl.app/library/vercel_wordmark_dark.svg"
+    });
+
+    public static CategoryString ExpectedCategoryString(string category)
+    {
+        return new CategoryString(category);
+    }
+
+    public static CategoryArray ExpectedCategoryArray = new(["Hosting", "Vercel"]);
+
+    public static string NoResultFoundMessage = "No SVG Found";
+
+    public static string NoResultFoundSubTitle(string query)
+    {
+        return $"Could not found {query} SVG";
+    }
+
+    public const string RequestLogo = "Request Logo";
+    public const string SubmitLogo = "Submit Logo";
 }
